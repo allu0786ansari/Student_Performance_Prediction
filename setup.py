02ccfaf3bcc388dfd3308dd7688766018134a139
Allaudin Ansari <allu456654ansari@gmail.com>
2025-01-27 21:15:44 +0530

    model_training is updated

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT ="-e ."
-def get_requirements(file_path:str)->List[str]:
-    '''
-    this function will return a list of package needed to install in the project
-    '''
-    
-    requirements =[]
-    with open('file_path') as file_obj:
+HYPEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This function returns a list of packages to install for the project.
+    """
+    requirements = []
+    with open(file_path) as file_obj:  # Corrected to use the file_path argument
         requirements = file_obj.readlines()
-        requirements = [req.replace("\n","") for req in requirements]
+        requirements = [req.strip() for req in requirements]  # Strip newlines
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
+
+    return requirements
 
 
 setup(
@@ -23,6 +25,5 @@ setup(
     author='Allaudin',
     author_email='allu456654ansari@gmail.com',
     packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
-
+    install_requires=get_requirements('requirements.txt')  # Pass the actual file path
 )
